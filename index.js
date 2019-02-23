07bd5db2301c36b37e9145eb6c800055e1852b55
Felice Forby <feliceforby@gmail.com>
2019-02-23 22:59:38 +0000

    Show repos and error message

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
 $(document).ready(function (){
+
 });
+const githubRoot = "https://api.github.com";
+
+function searchRepositories() {
+  let searchTerms = $('#searchTerms').val();
+  console.log("Searching Github for", searchTerms);
+
+  let url = `${githubRoot}/users/${searchTerms}/repos`;
+
+  $.get(url, function(data) {
+    console.log(data);
+    showRepositories(data);
+  }).fail(function(error) {
+    displayError(error);
+  });
+}
+
+function showRepositories(data) {
+  let repoList = data.map(repo =>
+    "<div>" +
+    `<h2><a href="${repo.html_url}" target="_blank">${repo.name}</a></h2>` +
+    `<p>Description: ${repo.description}</p>` +
+    `<a href="https://github.com/${repo.owner.login}" target="_blank"><img src="${repo.owner.avatar_url}" height="32" width="32" /> ${repo.owner.login}</a><br>` +
+    '<a href="#" onclick="showCommits()">Show Commits</a>' +
+    "</div>"
+  ).join('');
+
+  $("#results").html('<ul>' + repoList + '</ul>');
+}
+
+function showCommits() {
+
+}
+
+function displayError(error) {
+  let errorDiv = $("#errors");
+  errorDiv.html('There was an error:' + error.statusText);
+}
