3df52918ac1f63957d58030132e3299d5e32d86b
Felice Forby <feliceforby@gmail.com>
2019-02-24 05:35:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,19 +17,14 @@ function searchRepositories() {
   });
 }
 
-function renderRepository(repo) {
-  return
-  "<div>" +
-  `<h2><a href="${repo.html_url}" target="_blank">${repo.name}</a></h2>` +
-  `<p>Description: ${repo.description}</p>` +
-  `<a href="https://github.com/${repo.owner.login}" target="_blank"><img src="${repo.owner.avatar_url}" height="32" width="32" /> ${repo.owner.login}</a><br>` +
-  `<a href="#" data-repository="${repo.name}" data-owner="${repo.owner.login}" onclick="showCommits(this)">Show Commits</a>` +
-  "</div>";
-}
-
 function showRepositories(data) {
   const repoList = data.items.map(repo =>
-    return renderRepository(repo);
+    "<div>" +
+    `<h2><a href="${repo.html_url}" target="_blank">${repo.name}</a></h2>` +
+    `<p>Description: ${repo.description}</p>` +
+    `<a href="https://github.com/${repo.owner.login}" target="_blank"><img src="${repo.owner.avatar_url}" height="32" width="32" /> ${repo.owner.login}</a><br>` +
+    `<a href="#" data-repository="${repo.name}" data-owner="${repo.owner.login}" onclick="showCommits(this)">Show Commits</a>` +
+    "</div>"
   ).join('');
 
   $("#results").html('<ul>' + repoList + '</ul>');
