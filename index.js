243c8816dbde770d4f9fe47627eb153958a19637
Felice Forby <feliceforby@gmail.com>
2019-02-24 05:36:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-$(document).ready(function (){
-
-});
-const githubRoot = "https://api.github.com";
-
 function searchRepositories() {
-  let searchTerms = $('#searchTerms').val();
+  // Clear errors, if any
+  $("#errors").html("");
+
+  // Get search term from input and create API search url
+  const searchTerms = $('#searchTerms').val();
+  const url = `https://api.github.com/search/repositories?q=${searchTerms}`;
+
   console.log("Searching Github for", searchTerms);
 
-  let url = `${githubRoot}/users/${searchTerms}/repos`;
-
+  // Get data from Github's API. Display error if error occurs
   $.get(url, function(data) {
     console.log(data);
     showRepositories(data);
@@ -18,23 +18,41 @@ function searchRepositories() {
 }
 
 function showRepositories(data) {
-  let repoList = data.map(repo =>
+  const repoList = data.items.map(repo =>
     "<div>" +
     `<h2><a href="${repo.html_url}" target="_blank">${repo.name}</a></h2>` +
     `<p>Description: ${repo.description}</p>` +
     `<a href="https://github.com/${repo.owner.login}" target="_blank"><img src="${repo.owner.avatar_url}" height="32" width="32" /> ${repo.owner.login}</a><br>` +
-    '<a href="#" onclick="showCommits()">Show Commits</a>' +
+    `<a href="#" data-repository="${repo.name}" data-owner="${repo.owner.login}" onclick="showCommits(this)">Show Commits</a>` +
     "</div>"
   ).join('');
 
   $("#results").html('<ul>' + repoList + '</ul>');
 }
 
-function showCommits() {
+function showCommits(repoElement) {
+  const username = repoElement.dataset.owner;
+  const repo = repoElement.dataset.repository
+  const commitURL = `https://api.github.com/repos/${username}/${repo}/commits`;
 
+  $.get(commitURL, function(data) {
+    let commitList = data.map(commit =>
+      `<h3>${commit.commit.message}</h3>` +
+      `<p>SHA: ${commit.sha}</p>` +
+      `<p><img src="${commit.author.avatar_url}" height="32" width="32" /> ${commit.author.login} ${commit.commit.author.name}</p>`
+    ).join('');
+
+    $("#details").html(commitList);
+  }).fail(function (error) {
+    displayError(error);
+  });
 }
 
-function displayError(error) {
+function displayError() {
   let errorDiv = $("#errors");
-  errorDiv.html('There was an error:' + error.statusText);
+  errorDiv.html("I'm sorry, there's been an error. Please try again.");
 }
+
+$(document).ready(function (){
+
+});
